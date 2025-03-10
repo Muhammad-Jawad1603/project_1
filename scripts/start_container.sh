0068d2d17ffadfe00d0d76420844dc1b7faec665
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-03-10 10:34:21 +0500

    changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "Pulling latest image from ECR..."
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
+
 IMAGE_URI=$(cat /home/ubuntu/deploy/imagedefinitions.json | jq -r '.[0].imageUri')
 
+echo "Stopping any existing container: hello-world-node..."
+docker stop hello-world-node || true
+docker rm hello-world-node || true
+
 echo "Running new container..."
-docker run -d --name my-app -p 3000:3000 $IMAGE_URI
+docker run -d --name hello-world-node -p 3000:3000 $IMAGE_URI
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 CONTAINER_ID=$(docker ps -q --filter "name=hello-world-node")
+
 if [ -n "$CONTAINER_ID" ]; then
-  echo "Stopping running container..."
+  echo "Stopping running container: hello-world-node..."
   docker stop $CONTAINER_ID
   docker rm $CONTAINER_ID
 else
-  echo "No running container found."
+  echo "No running container found with name: hello-world-node."
 fi
