ad4ac6ca330f03a1f2de6e313bda9dc200563aa5
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-03-10 14:02:39 +0500

    changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-echo "Script started at $(date)" | sudo tee -a /home/ubuntu/deploy/scripts/debug.log
-
-echo "Pulling latest image from ECR..." | sudo tee -a /home/ubuntu/deploy/scripts/debug.log
+echo "Pulling latest image from ECR..."
 sudo aws ecr get-login-password --region eu-west-1 | sudo docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
 
 IMAGE_URI=$(cat /home/ubuntu/deploy/imagedefinitions.json | jq -r '.[0].imageUri')
-echo "Using image: $IMAGE_URI" | sudo tee -a /home/ubuntu/deploy/scripts/debug.log
+echo "Using image: $IMAGE_URI"
 
-echo "Stopping any existing container..." | sudo tee -a /home/ubuntu/deploy/scripts/debug.log
+echo "Stopping old container..."
 sudo docker stop hello-world-node || true
 sudo docker rm hello-world-node || true
 
-echo "Starting new container..." | sudo tee -a /home/ubuntu/deploy/scripts/debug.log
-sudo docker run -d --name hello-world-node -p 3000:3000 $IMAGE_URI
+echo "Removing old Docker images..."
+sudo docker rmi -f $(sudo docker images -q) || true
 
-echo "Script finished at $(date)" | sudo tee -a /home/ubuntu/deploy/scripts/debug.log
+echo "Pulling the latest image..."
+sudo docker pull $IMAGE_URI
+
+echo "Starting new container..."
+sudo docker run -d --name hello-world-node -p 3000:3000 $IMAGE_URI
