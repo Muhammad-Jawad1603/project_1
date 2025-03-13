e6bb106e3f363d8cbea43d02c173dcb0cde31e6a
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-03-13 09:29:58 +0500

    changes

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get("/api2", (req, res) => {
-    res.send('<h1>Node application on Ec2</h1><br><p>THIS IS THE NEW PIPELINE</P>');
+    res.send('<h1>Node application on Ec2</h1><br><p>THIS IS THE NEW PIPELINE7860</P>');
   });
 
 app.get('/', (req, res) => {
-    res.send('<h1>Node application on EC2</h1><br><p>THIS IS THE NEW PIPELINE</P>');
+    res.send('<h1>Node application on EC2</h1><br><p>THIS IS THE NEW PIPELINE7860</P>');
 });
 
 const PORT = 3000;
