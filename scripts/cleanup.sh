14f81a0ec0e09074575d09dd924edcbda4b79369
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-03-18 13:05:45 +0500

    error fixing

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
+set -e
 
-APP_DIR="/home/ubuntu/node-app"
-TEMP_DIR="/home/ubuntu/temp"
-
-echo "Removing old application files..."
-sudo rm -rf ${APP_DIR:?}/*  # Prevent accidental removal of /home/ubuntu if APP_DIR is empty
-
+sudo rm -rf /home/ubuntu/node-app/*
 echo "Moving new files to application directory..."
-shopt -s dotglob  # Ensure hidden files are included
-sudo mv ${TEMP_DIR}/* ${APP_DIR}/
+sudo cp /home/ubuntu/temp/* /home/ubuntu/node-app/
 
 echo "Deployment files moved successfully."
 
-echo "Cleaning up temporary files..."
-sudo rm -rf ${TEMP_DIR:?}/*
-
-echo "Cleanup completed successfully."
-
+sudo rm -rf /home/ubuntu/temp/*
