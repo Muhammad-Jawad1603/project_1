50163e2ebf452911ac3489e2b7abc4c750b6bba4
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-03-18 12:33:09 +0500

    changes

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,8 +3,8 @@ set -e
 
 sudo rm -rf /home/ubuntu/node-app/
 echo "Moving new files to application directory..."
-sudo mv /home/ubuntu/temp /home/ubuntu/node-app
+sudo mv /home/ubuntu/temp/* /home/ubuntu/node-app
 
 echo "Deployment files moved successfully."
 
-sudo rm -rf /home/ubuntu/temp/
+sudo rm -rf /home/ubuntu/temp/*
