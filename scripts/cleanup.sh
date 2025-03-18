48d4c8615598f809e71cc0b5642843955d63e7b7
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-03-18 12:36:04 +0500

    error fixed hopefully

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sudo rm -rf /home/ubuntu/node-app/
+sudo rm -rf /home/ubuntu/node-app/*
 echo "Moving new files to application directory..."
 sudo mv /home/ubuntu/temp/* /home/ubuntu/node-app
 
