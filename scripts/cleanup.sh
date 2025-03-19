9dbb20860aef3a024cbd6fdf65220564ed4d99eb
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-03-19 10:19:39 +0500

    changes to index and cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,6 +4,8 @@ set -e
 sudo rm -rf /home/ubuntu/node-app/*
 echo "Moving new files to application directory..."
 sudo cp -r /home/ubuntu/temp/* /home/ubuntu/node-app/
+sudo docker rmi -f $(docker images -aq)
+
 
 echo "Deployment files moved successfully."
 
